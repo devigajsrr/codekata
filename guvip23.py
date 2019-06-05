410534e041660c71252bafd864aa370d0814a1bd
devigajsrr <50616391+devigajsrr@users.noreply.github.com>
2019-06-05 10:49:06 +0530

    Add files via upload

---
+++ guvip23.py
@@ -0,0 +1,10 @@
+n,k=map(int,input().split())
+o=input()
+list1=[int(x) for x in input().split()]
+list2=[int(x) for x in input().split()]
+s=[]
+for iter in range(len(list2)):
+	list1.append(list2[iter])
+	x=max(list1)
+	s.append(x)
+print(*s)
