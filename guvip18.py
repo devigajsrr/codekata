9e20d2cd545a28a9802a6e8fba68891ef78dc11d
devigajsrr <50616391+devigajsrr@users.noreply.github.com>
2019-06-05 10:25:26 +0530

    Add files via upload

---
+++ guvip18.py
@@ -0,0 +1,14 @@
+n=int(input())
+arr=[]
+asc=0
+count=0
+for i in range(n):
+    c=input()
+    arr.append(c)
+for i in arr:
+	for j in i:
+		asc+=ord(j)
+	if(asc==612):
+		count+=1
+	asc=0
+print(count)
