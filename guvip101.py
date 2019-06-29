8ad62f5ac05f862ed1a74ea6ff4f2f58722b6bc2
devigajsrr <50616391+devigajsrr@users.noreply.github.com>
2019-06-29 11:08:40 +0530

    Add files via upload

---
+++ guvip101.py
@@ -0,0 +1,15 @@
+try:
+	m=int(input())
+	arr=list(map(int,input().split()))
+	sum=0
+	a=[]
+	for i in range(0,m-1):
+		if(arr[i]<arr[i+1]):
+			a.append(arr[i+1])
+		else:
+			a.append(arr[i])
+	for j in range(0,len(a)):
+		sum=sum+a[j]
+	print(sum)
+except ValueError:
+	print("invalid")
