b4e64628354c91d530d3afda7fabd0d3650929d1
devigajsrr <50616391+devigajsrr@users.noreply.github.com>
2019-06-29 09:58:10 +0530

    Add files via upload

---
+++ guvip99.py
@@ -0,0 +1,14 @@
+m=list(input())
+d=[]
+for i in range(len(m)-1,-1,-3):
+    w=i
+    s=0
+    j=0
+    f=1
+    while j!=3 and w>=0:
+        if j!=0: f=(2 ** j)
+        s = s + ( f* int(m[w]))
+        j=j+1
+        w=w-1
+    d.append((s))
+for j in range(len(d)-1,-1,-1):print(int(d[j]),end="")
