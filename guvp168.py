468202e534d2b9e17a756e5147537ecc67c3de3e
devigajsrr <50616391+devigajsrr@users.noreply.github.com>
2019-07-20 16:56:40 +0530

    Add files via upload

---
+++ guvp168.py
@@ -0,0 +1,8 @@
+n=list(map(str,input()))
+d=[]
+o=['1','2','3','4','5','6','7','8','9','0']
+for i in range(0,len(n)):
+    if n[i] not in o: d.append(n[i])
+    else: d.append(n[i-1]*(int(n[i])-1))
+s="".join(d)
+print(s
